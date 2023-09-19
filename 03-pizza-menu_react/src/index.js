55d8393a48e4e97ebde090af353a513a75358429
s-itov <simeon.itov@gmail.com>
2023-09-19 11:59:26 +0300

    adding reusable components

--- 03-pizza-menu_react/src/index.js
+++ 03-pizza-menu_react/src/index.js
@@ -2,7 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function App() {
-    return <h1>Hello from react</h1>
+    return (
+        <>
+            <h1>Hello from react</h1>
+            <Pizza />
+            <Pizza />
+            <Pizza />
+        </>
+    );
+}
+
+function Pizza() {
+
+    return (
+        <>
+            <img src="pizzas/spinaci.jpg" alt='Pizza spinaci'></img>
+            <h2>Pizza Spinaci</h2>
+            <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+        </>
+    );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
