befed1b9957749b81b4aec3b73487d8ce2855fb2
s-itov <simeon.itov@gmail.com>
2023-09-20 11:48:24 +0300

    adding more components and styling them

--- 03-pizza-menu_react/src/index.js
+++ 03-pizza-menu_react/src/index.js
@@ -1,23 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 function App() {
     return (
-        <>
-            <h1>Hello from react</h1>
+        <div className='container'>
+            <Header />
+            <Menu />
+            <Footer />
+        </div>
+    );
+}
+
+function Header() {
+    return (
+        <header className='header'>
+            <h1>Fast React Pizza Co.</h1>
+        </header>
+    );
+}
+
+function Menu() {
+    return (
+        <main className='menu'>
+            <h2>Our menu</h2>
             <Pizza />
             <Pizza />
             <Pizza />
-        </>
+        </main>
+    );
+}
+
+function Footer() {
+    return (
+        <footer className='footer'>{new Date().toLocaleTimeString()}. We're currently open</footer>
     );
 }
 
 function Pizza() {
-
     return (
         <>
             <img src="pizzas/spinaci.jpg" alt='Pizza spinaci'></img>
-            <h2>Pizza Spinaci</h2>
+            <h3>Pizza Spinaci</h3>
             <p>Tomato, mozarella, spinach, and ricotta cheese</p>
         </>
     );
