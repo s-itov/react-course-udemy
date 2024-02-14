b3eedc894ba5ec70de0ca675563a80121676a9f4
s-itov <simeon.itov@gmail.com>
2024-02-14 16:30:54 +0200

    conditional open and close with another state

--- 04-steps/steps/src/App.js
+++ 04-steps/steps/src/App.js
@@ -9,6 +9,7 @@ const messages = [
 export default function App() {
 
     const [step, setStep] = useState(1);
+    const [isOpen, setIsOpen] = useState(false);
 
     function handlePrevious() {
         if (step > 1) {
@@ -22,19 +23,25 @@ export default function App() {
         }
     }
 
-    return <div className="steps">
-        <div className="numbers">
-            <div className={step >= 1 ? "active" : ""}>1</div>
-            <div className={step >= 2 ? "active" : ""}>2</div>
-            <div className={step >= 3 ? "active" : ""}>3</div>
-        </div>
+    return (
+        <>
+            {isOpen && (
+                <div className="steps">
+                    <div className="numbers">
+                        <div className={step >= 1 ? "active" : ""}>1</div>
+                        <div className={step >= 2 ? "active" : ""}>2</div>
+                        <div className={step >= 3 ? "active" : ""}>3</div>
+                    </div>
 
-        <p className="message">Step {step} : {messages[step - 1]}</p>
+                    <p className="message">Step {step} : {messages[step - 1]}</p>
 
-        <div className="buttons">
-            <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handlePrevious}>Previous</button>
-            <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handleNext}>Next</button>
-        </div>
+                    <div className="buttons">
+                        <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handlePrevious}>Previous</button>
+                        <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handleNext}>Next</button>
+                    </div>
+                </div>
+            )}
+        </>
 
-    </div>;
+    )
 }
